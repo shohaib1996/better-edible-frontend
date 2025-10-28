7d2fe31a7b3ad7ef931cc042911c9c4a1ec0afbb
Shohaib hossain <khanshohaibhossain@gmail.com>
2025-10-28 22:58:43 +0600

    resolve the issue

--- src/app/timeclock/page.tsx
+++ src/app/timeclock/page.tsx
@@ -1,7 +1,7 @@
-export const dynamic = 'force-dynamic'
 "use client"
 
 
+export const dynamic = 'force-dynamic'
 import { useState } from "react";
 import { toast } from "sonner";
 import { Card, CardContent, CardHeader, CardTitle } from "@/src/components/ui/card";
